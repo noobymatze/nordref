9b0962a537502b0ebea1ac1a0bb67e226626cf30
Matthias Metzger <noobymatze@yahoo.de>
2019-12-26 12:28:46 +0100

    add docs in directory for github pages

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule Nordref.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      docs: "docs -o docs/"
     ]
   end
 
